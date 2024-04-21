5531b9dced661f71ff35f43e343d8c11922e79a8
Thales <trmendes0@gmail.com>
2024-04-21 07:05:36 -0300

    atualizacao requests

--- utils/requests/request.py
+++ utils/requests/request.py
@@ -6,31 +6,6 @@ from utils.config import url_abas
 
 url = 'http://vitibrasil.cnpuv.embrapa.br/index.php?'
 year_list = [f'ano={x}' for x in range(1970, 2023)]
-params='opcao=opt_03'
-
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-# response = requests.get(url+params, headers=headers)
-
-# http://vitibrasil.cnpuv.embrapa.br/index.php?ano=1999&opcao=opt_03&subopcao=subopt_01
-# http://vitibrasil.cnpuv.embrapa.br/index.php?ano=1970&opcao=opt_03&subopcao=subopt_02
-
-# if response.status_code == 200:
-#     print(f'r.headers: {response.headers}')
-#     print(f'r.url: {response.url}')
-# else:
-#     print('fail')
-
-# soup = BeautifulSoup(response.text, 'html.parser')
-# filter = soup.thead.parent
-
-
-#headers
-# print(filter.find_all('th')[0].string) 
-# print(filter.find_all('th')[1].string)
-
-# valores
-# for i in range(len(filter.find_all('td'))):
-    # print(filter.find_all('td')[i].string.strip())
 
 
 '''
@@ -40,19 +15,52 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 4. entrar no <tbody> depois <tr> e buscar o produto e subproduto, sendo <td class='tb_item'> com dois valores
 5. ainda no <tbody> depois <tr> e buscar o produto e subproduto, sendo <td class='tb_subitem'> com dois valores
 6. entender que quando o valor de td class='tb_item', então, produto. Se 'tb_subitem', então, subproduto
+
+# http://vitibrasil.cnpuv.embrapa.br/index.php?ano=1999&opcao=opt_03&subopcao=subopt_01
+# http://vitibrasil.cnpuv.embrapa.br/index.php?ano=1970&opcao=opt_03&subopcao=subopt_02
 '''
+
+def get_request(url):
+    
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    try:
+        response = requests.get(url, headers=headers)
+        
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            filter = soup.thead.parent
+            print('Sucess Connection')
+            
+            print(filter.find_all('th')[0].string) 
+            print(filter.find_all('th')[1].string)
+            
+            for i in range(len(filter.find_all('td'))):
+                print(filter.find_all('td')[i].string.strip())
+    
+            # return soup.thead.parent
+        else: 
+            print('Fail Connection')
+            
+    except Exception as e:
+        print(f'Fail Connection: Error - {e}')
 
 
 def producao(url, url_abas, year_list):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    # retorna uma lista das urls a serem consultadas do topico de producao
+    return [ url+ year_list[x] + url_abas['producao'] for x in range(len(year_list))]
+
+
+def get_data():
     
+    urls = producao(url, url_abas, year_list)
     
-    # response = requests.get(url+params, headers=headers)
-
-    for x in range(len(year_list)):
-        print(url + year_list[x] + url_abas['producao']) 
-
-
-producao(url, url_abas, year_list)
-
+    for i in range(len(urls)):
+        get_request(urls[i])
+      
+if __name__ == '__main__':
+    # print(producao(url, url_abas, year_list))
+    # get_request(url)
+    get_data()
+    
 
